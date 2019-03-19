ad161b26e50d988870ed6967277c8b5d7a86b95a
dunck <duncanck0@gmail.com>
2019-03-19 18:53:19 +1000

    Basic 2019 code from existing repository.

---
+++ jetson/video/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp
+// Griffith Droid Racer 2019
+
+#include <opencv2/opencv.hpp>
+
+int capDev = 0;
+
+VideoCapture cap(capDev); // open the default camera
+
+int main()
+{
+    return 0;
+}
